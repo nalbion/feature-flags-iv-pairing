73b2581892528b68c83e34992222231b59c7d576
Janis Ardel <jardel@au.palo-it.com>
2021-09-29 21:48:44 +1000

    feature-flag challenge

--- src/service/FeatureFlagServiceImpl.ts
+++ src/service/FeatureFlagServiceImpl.ts
@@ -1,51 +1,25 @@
 import {FeatureFlagService} from "./FeatureFlagService";
 import {FeatureFlag} from "../domain/FeatureFlag";
 
-enum Type {
-    Permitted,
-    Prohibited
-}
-
 export class FeatureFlagServiceImpl implements FeatureFlagService {
 
     //TODO: Change type any
-    private featureFlags: Map<FeatureFlag, Map<Type, string[]>>;
+    private featureFlags: any;
 
     constructor() {
-        this.featureFlags = new Map<FeatureFlag, Map<Type, string[]>>()
     }
 
     permitUser(userId: string, feature: FeatureFlag): void {
-        this.populateIfNotExisting(feature)
-        const users = this.featureFlags.get(feature)
-
-        const permittedUsers = users.get(Type.Permitted)
-        const prohibitedUsers = users.get(Type.Prohibited)
-
-        const desiredPercentage = feature.percentagePermitted;
-        const currentPercentage = 100 * permittedUsers.length / (permittedUsers.length + prohibitedUsers.length)
-
-        if (currentPercentage < desiredPercentage) {
-            permittedUsers.push(userId)
-        } else {
-            prohibitedUsers.push(userId)
-        }
+        // TODO 1: permit user [userId] for the feature [feature]
     }
 
     isUserPermitted(userId: string, feature: FeatureFlag): boolean {
-        return this.featureFlags.get(feature).get(Type.Permitted).includes(userId)
+        //TODO 2: Return if user [userId] is permitted to access the feature [feature]
+        return false
     }
 
     getPermittedUsers(feature: FeatureFlag): string[] {
-        return this.featureFlags.get(feature).get(Type.Permitted)
-    }
-
-    private populateIfNotExisting(feature: FeatureFlag) {
-        if (!this.featureFlags.get(feature)) {
-            const users = new Map<Type, string[]>()
-            users.set(Type.Permitted, [])
-            users.set(Type.Prohibited, [])
-            this.featureFlags.set(feature, users)
-        }
+        // TODO 3: Get all permitted users [string[]] for the feature [feature]
+        return []
     }
 }
